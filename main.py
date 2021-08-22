8deddab1a0796d52393dbc3e2e7d64baf932cd0d
Jayant Shrivastava <jshrivastava03@gmail.com>
2021-08-22 00:03:17 +0000

    db: transition to sqlite

--- main.py
+++ main.py
@@ -8,45 +8,60 @@ import os
 import random
 from pony.orm import Database, PrimaryKey, Required, sql_debug, db_session
 
+# Create a Flask server.
+app = Flask(__name__)
+
 """
 Part 7 - Working with A Database
 """
-# # Source: https://www.cockroachlabs.com/docs/stable/build-a-python-app-with-cockroachdb-pony.html
-# db = Database()
+# Source: https://www.cockroachlabs.com/docs/stable/build-a-python-app-with-cockroachdb-pony.html
+db = Database()
 
-# db_password = os.environ['db_password']
-# # print(len(db_password))
+class Book(db.Entity):
+  _table_ = 'books'
+  id = PrimaryKey(int)
+  title = Required(str)
+  author = Required(str)
+  rating = float
+  pages = int
 
-# class Book(db.Entity):
-#   _table_ = 'books'
-#   id = PrimaryKey(int)
-#   title = Required(str)
-#   author = Required(str)
-#   rating = float
-#   pages = int
+# SQLite
+# Store data in a file!
+db_params = dict(provider='sqlite', filename='booksdb.sqlite', create_db=True) # The create_db option will create the file if it does not exist.
 
-# db_params = dict(provider='cockroach', user='jayant', host='free-tier.gcp-us-central1.cockroachlabs.cloud', port=26257, database='upbeat-bear-3029.defaultdb', password=db_password)
+# CockroachDB 
+# https://www.cockroachlabs.com/free-tier/
+# db_params = dict(provider='cockroach', user='jayant', host='free-tier.gcp-us-central1.cockroachlabs.cloud', port=26257, database='ample-lemur-3072.defaultdb', password=<INSERT_PASSWORD>)
+# Note: You may want to use a Replit environment variables to store the password and database name (see https://docs.replit.com/tutorials/08-storing-secrets-and-history). Note that os.getenv/os.envrion may not work outside of main.py.
 
-# sql_debug(True)  # Print all generated SQL queries to stdout
-# db.bind(**db_params)  # Bind Database object to the real database
-# db.generate_mapping(create_tables=True)  # Create tables
+sql_debug(True)  # Print all generated SQL queries to stdout
+db.bind(**db_params)  # Bind Database object to the real database
+db.generate_mapping(create_tables=True)  # Create tables
 
-# @db_session  # db_session decorator manages the transactions
-# def db_create_book(id, title, author, rating, pages):
-#   return Book(id, title, author, rating, pages).to_dict()
+@db_session  # db_session decorator manages the transactions
+def db_create_book(id, title, author, rating, pages):
+  return Book(id, title, author, rating, pages).to_dict()
 
-# @db_session
-# def db_get_all_books():
-#   return [book.to_dict() for book in Book.select()]
+@db_session
+def db_get_all_books():
+  return [book.to_dict() for book in Book.select()]
 
-# @db_session 
-# def db_get_book(id, title, author, rating, pages):
-#   return Book(id, title, author, rating, pages).to_dict() 
+@db_session 
+def db_get_book(id, title, author, rating, pages):
+  return Book(id, title, author, rating, pages).to_dict() 
 
-# @app.route('/', methods=['GET'])
-# @db_session
-# def index():
-#     return jsonify(db_get_all_books())
+@app.route('/', methods=['GET'])
+@db_session
+def index():
+    return jsonify(db_get_all_books())
+
+@app.route("/<id>", methods=['GET'])
+@db_session
+def get_book(id):
+  book = Book.get(id=id)
+  if not book:
+     return jsonify({"error": "invalid id"})
+  return jsonify(Book.get(id=id).to_dict())
 
 """
 End of Part 7 Code
@@ -57,83 +72,84 @@ End of Part 7 Code
 Parts 2-5 - Building an API without a Database
 """
 
-# Part 2 - Set up an API
-app = Flask(__name__)
+# # Books stored in memory in a Python dictionary
+# books = {
+#   0: {'id': 0,
+#     'title': 'Harry Potter and the Chamber of Secrets (Harry Potter #2)',
+#     'author': 'J.K. Rowling',
+#     'rating': 4.42,
+#     'pages': 352},
+#   1: {'id': 1,
+#     'title': 'The Fellowship of the Ring (The Lord of the Rings #1)',
+#     'author': 'J.R.R. Tolkien',
+#     'rating': 4.36,
+#     'pages': 398},
+#   2: {'id': 2,
+#     'title': 'Treasure Island',
+#     'author': 'Robert Louis Stevenson',
+#     'rating': 3.83,
+#     'pages': 311}
+# }
 
-# Books stored in memory in a Python dictionary
-books = {
-  0: {'id': 0,
-    'title': 'Harry Potter and the Chamber of Secrets (Harry Potter #2)',
-    'author': 'J.K. Rowling',
-    'rating': 4.42,
-    'pages': 352},
-  1: {'id': 1,
-    'title': 'The Fellowship of the Ring (The Lord of the Rings #1)',
-    'author': 'J.R.R. Tolkien',
-    'rating': 4.36,
-    'pages': 398},
-  2: {'id': 2,
-    'title': 'Treasure Island',
-    'author': 'Robert Louis Stevenson',
-    'rating': 3.83,
-    'pages': 311}
-}
+# # Part 3 - Get Requests
+# @app.route('/', methods=['GET'])
+# def index():
+#     return jsonify([books[book_id] for book_id in books])
 
-# Part 3 - Get Requests
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify([books[book_id] for book_id in books])
+# # Request Params Example
+# @app.route("/<id>", methods=['GET'])
+# def get_book(id):
+#     if int(id) not in books:
+#       return jsonify({"error": "invalid id"})
+#     return jsonify(books[int(id)])
 
-# Request Params Example
-@app.route("/<id>", methods=['GET'])
-def get_book(id):
-    if int(id) not in books:
-      return jsonify({"error": "invalid id"})
-    return jsonify(books[int(id)])
+# # Route Params Example
+# @app.route("/search", methods=['GET'])
+# def search_books():
+#   result = []
 
-# Route Params Example
-@app.route("/search", methods=['GET'])
-def search_books():
-  result = []
+#   for book_id, book in books.items():
+#     if request.args.get('max_pages'):
+#       if book['pages'] > int(request.args.get('max_pages')):
+#         continue
 
-  for book_id, book in books.items():
-    if request.args.get('max_pages'):
-      if book['pages'] > int(request.args.get('max_pages')):
-        continue
+#     if request.args.get('min_rating'):
+#       if book['rating'] < int(request.args.get('min_rating')):
+#         continue
 
-    if request.args.get('min_rating'):
-      if book['rating'] < int(request.args.get('min_rating')):
-        continue
+#     result.append(book)
+#   return jsonify(result)
 
-    result.append(book)
-  return jsonify(result)
+# # Part 4 - Post Requests
+# @app.route("/", methods=['POST'])
+# def create_book():
+#   new_book = request.json
+#   if new_book['id'] in books:
+#     return jsonify({"error": "book with that id already exists"})
 
-# Part 4 - Post Requests
-@app.route("/", methods=['POST'])
-def create_book():
-  new_book = request.json
-  if new_book['id'] in books:
-    return jsonify({"error": "book with that id already exists"})
+#   books[new_book['id']] = new_book
+#   return jsonify(new_book)
 
-  books[new_book['id']] = new_book
-  return jsonify(new_book)
+# # Part 5 - Put and Delete Requests
+# @app.route("/<id>", methods=['PUT'])
+# def update_author(id):
+#   if int(id) not in books:
+#     return jsonify({"error": "invalid id"})
+#   books[int(id)]['author'] = request.json['author']
 
-# Part 5 - Put and Delete Requests
-@app.route("/<id>", methods=['PUT'])
-def update_author(id):
-  if int(id) not in books:
-    return jsonify({"error": "invalid id"})
-  books[int(id)]['author'] = request.json['author']
+#   return jsonify(books[int(id)])
 
-  return jsonify(books[int(id)])
+# @app.route("/<id>", methods=['DELETE'])
+# def delete_book(id):
+#   if int(id) not in books:
+#     return jsonify({"error": "invalid id"})
+#   deleted_book = books[int(id)]
+#   del books[int(id)]
+#   return jsonify(deleted_book)
 
-@app.route("/<id>", methods=['DELETE'])
-def delete_book(id):
-  if int(id) not in books:
-    return jsonify({"error": "invalid id"})
-  deleted_book = books[int(id)]
-  del books[int(id)]
-  return jsonify(deleted_book)
+"""
+End of Parts 2-5
+"""
 
 # Runs the API and exposes it on https://<repl name>.<replit username>.repl.co
 # ex. Mine deploys to https://htn-api.jayantsh.repl.co.
--- seed_data.py
+++ seed_data.py
@@ -2,7 +2,7 @@ import random
 import os
 from pony.orm import Database, PrimaryKey, Required, sql_debug, db_session
 
-db_password = '<PUT DB PASSWORD HERE>' # Replit does not read os.environ['db_password'] correctly outside of main.py :(
+db_password = 'slJ9J1ABhGg1eic3'
 
 db = Database()
 
@@ -14,7 +14,14 @@ class Book(db.Entity):
   rating = Required(float)
   pages = Required(int)
 
-db_params = dict(provider='cockroach', user='jayant', host='free-tier.gcp-us-central1.cockroachlabs.cloud', port=26257, database='upbeat-bear-3029.defaultdb', password=db_password)
+# SQLite
+# Store data in a file!
+db_params = dict(provider='sqlite', filename='booksdb.sqlite', create_db=True) # The create_db option will create the file if it does not exist.
+
+# CockroachDB 
+# https://www.cockroachlabs.com/free-tier/
+# db_params = dict(provider='cockroach', user='jayant', host='free-tier.gcp-us-central1.cockroachlabs.cloud', port=26257, database='ample-lemur-3072.defaultdb', password=<INSERT_PASSWORD>)
+# Note: You may want to use a Replit environment variables to store the password and database name (see https://docs.replit.com/tutorials/08-storing-secrets-and-history). Note that os.getenv/os.envrion may not work outside of main.py.
 
 sql_debug(True)  # Print all generated SQL queries to stdout
 db.bind(**db_params)  # Bind Database object to the real database
@@ -26,6 +33,6 @@ def create_book(id,title, author, rating, pages):
 
 # Read CSV Data and Create Books
 f = open("books.csv", "r")
-for i,line in enumerate(f):
+for line in f:
   parts = line.strip().split(',')
-  create_book(i,parts[0], parts[1], parts[2], parts[3])
+  create_book(parts[0], parts[1], parts[2], float(parts[3]), parts[4])
